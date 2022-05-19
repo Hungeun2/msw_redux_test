b4705125c75c249d351d1de48711750d0877439b
Hungeun2 <bepoohun@gmail.com>
2022-05-19 19:35:05 +0900

    feat: add redux-test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import './App.css';
-import TestMocking from './components/TestMocking';
+// import TestMocking from './components/TestMocking';
+import Counter from './features/counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      <TestMocking />
+      {/* <TestMocking /> */}
+      <Counter />
     </div>
   );
 }
